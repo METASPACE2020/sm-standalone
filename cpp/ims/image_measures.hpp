302afe2ddd535d055a1d4cc19f04d20691b0d6fe
Artem Tarasov <artem.tarasov@embl.de>
2016-02-09 10:15:41 +0100

    tabs->spaces

--- cpp/ims/image_measures.hpp
+++ cpp/ims/image_measures.hpp
@@ -12,13 +12,13 @@ namespace ims {
 
   double isotopeImageCorrelation(const ims::ImageF* images, size_t n,
                                  const ms::IsotopePattern& pattern);
-  
+
   double isotopeImageCorrelation(const std::vector<ims::ImageF>& images,
                                  const ms::IsotopePattern& pattern);
 
   double isotopePatternMatch(const ims::ImageF* images, size_t n,
                              const ms::IsotopePattern& pattern);
 
-	double isotopePatternMatch(const std::vector<ims::ImageF>& images,
-                             const ms::IsotopePattern& pattern);
+  double isotopePatternMatch(const std::vector<ims::ImageF>& images,
+      const ms::IsotopePattern& pattern);
 }
--- cpp/imzb/reader.hpp
+++ cpp/imzb/reader.hpp
@@ -17,130 +17,130 @@
 namespace imzb {
 
 class ImzbReader {
-	std::string fn_;
-	std::ifstream in_;
+  std::string fn_;
+  std::ifstream in_;
 
-	IndexPtr index_;
-	size_t block_idx_;
+  IndexPtr index_;
+  size_t block_idx_;
 
-	std::vector<char> buffer_;
-	std::vector<ims::Peak> peaks_;
-	size_t n_peaks_;
-	size_t pos_;
+  std::vector<char> buffer_;
+  std::vector<ims::Peak> peaks_;
+  size_t n_peaks_;
+  size_t pos_;
 
   size_t decompressBlock(size_t block_idx,
                          std::ifstream& in,
                          std::vector<char>& inbuf,
                          std::vector<ims::Peak>& outbuf) const
-	{
-		uint64_t start = index_->offsets[block_idx];
-		uint64_t end = index_->offsets[block_idx + 1];
-		inbuf.resize(end - start);
-		in.seekg(start);
-		in.read(&inbuf[0], end - start);
-		return blosc_decompress_ctx(&inbuf[0], &outbuf[0],
-				outbuf.size() * sizeof(ims::Peak), 1) / sizeof(ims::Peak);
-	}
+  {
+    uint64_t start = index_->offsets[block_idx];
+    uint64_t end = index_->offsets[block_idx + 1];
+    inbuf.resize(end - start);
+    in.seekg(start);
+    in.read(&inbuf[0], end - start);
+    return blosc_decompress_ctx(&inbuf[0], &outbuf[0],
+        outbuf.size() * sizeof(ims::Peak), 1) / sizeof(ims::Peak);
+  }
 
-	bool readNextBlock() {
-		++block_idx_;
-		if (block_idx_ == index_->mzs.size()) {
-			n_peaks_ = 0;
-			return false;
-		}
+  bool readNextBlock() {
+    ++block_idx_;
+    if (block_idx_ == index_->mzs.size()) {
+      n_peaks_ = 0;
+      return false;
+    }
 
-		n_peaks_ = decompressBlock(block_idx_, in_, buffer_, peaks_);
-		pos_ = 0;
-		return true;
-	}
+    n_peaks_ = decompressBlock(block_idx_, in_, buffer_, peaks_);
+    pos_ = 0;
+    return true;
+  }
 
-	bool empty_;
+  bool empty_;
 
 public:
-	ImzbReader(const std::string& filename) :
-		fn_(filename),
-		in_(filename), block_idx_(0), peaks_(imzb::ImzbWriter::BLOCK_SIZE),
-		n_peaks_(0), pos_(0), empty_(false)
-	{
-		std::ifstream in_idx(filename + ".idx", std::ios::binary);
+  ImzbReader(const std::string& filename) :
+    fn_(filename),
+    in_(filename), block_idx_(0), peaks_(imzb::ImzbWriter::BLOCK_SIZE),
+    n_peaks_(0), pos_(0), empty_(false)
+  {
+    std::ifstream in_idx(filename + ".idx", std::ios::binary);
 
-		index_ = std::make_shared<Index>();
-		index_->read(in_idx);
+    index_ = std::make_shared<Index>();
+    index_->read(in_idx);
 
-		in_.seekg(0, in_.end);
-		index_->offsets.push_back(in_.tellg());
-		in_.seekg(0, in_.beg);
-	}
+    in_.seekg(0, in_.end);
+    index_->offsets.push_back(in_.tellg());
+    in_.seekg(0, in_.beg);
+  }
 
-	bool readNext(ims::Peak& peak) {
-		if (empty_)
-			return false;
+  bool readNext(ims::Peak& peak) {
+    if (empty_)
+      return false;
 
-		if (pos_ >= n_peaks_) {
-			if (!readNextBlock()) {
-				empty_ = true;
-				return false;
-			}
-		}
+    if (pos_ >= n_peaks_) {
+      if (!readNextBlock()) {
+        empty_ = true;
+        return false;
+      }
+    }
 
-		peak = peaks_[pos_];
-		++pos_;
-		return true;
-	}
+    peak = peaks_[pos_];
+    ++pos_;
+    return true;
+  }
 
-	void reset() {
-		in_.seekg(0, in_.beg);
-		n_peaks_ = pos_ = block_idx_ = 0;
-		empty_ = false;
-	}
+  void reset() {
+    in_.seekg(0, in_.beg);
+    n_peaks_ = pos_ = block_idx_ = 0;
+    empty_ = false;
+  }
 
-	std::vector<ims::Peak> slice(double min_mz, double max_mz) const {
-		assert(min_mz < max_mz);
-		std::vector<char> inbuf;
-		std::vector<ims::Peak> result, outbuf{imzb::ImzbWriter::BLOCK_SIZE};
-		size_t start_block = index_->startBlock(min_mz);
-		size_t end_block = index_->endBlock(max_mz);
-		std::ifstream in{fn_, std::ios::binary};
+  std::vector<ims::Peak> slice(double min_mz, double max_mz) const {
+    assert(min_mz < max_mz);
+    std::vector<char> inbuf;
+    std::vector<ims::Peak> result, outbuf{imzb::ImzbWriter::BLOCK_SIZE};
+    size_t start_block = index_->startBlock(min_mz);
+    size_t end_block = index_->endBlock(max_mz);
+    std::ifstream in{fn_, std::ios::binary};
 
-		for (size_t i = start_block; i < end_block; ++i) {
-			size_t n = decompressBlock(i, in, inbuf, outbuf);
-			auto beg = outbuf.cbegin(), end = outbuf.cbegin() + n;
-			if (outbuf.front().mz < min_mz)
-				beg = std::lower_bound(beg, end, min_mz,
-						[](const ims::Peak& p, double mz) { return p.mz < mz; });
-			if (outbuf.back().mz > max_mz)
-				end = std::upper_bound(beg, end, max_mz,
-						[](double mz, const ims::Peak& p) { return mz < p.mz; });
-			result.insert(result.end(), beg, end);
-		}
-		return result;
-	}
+    for (size_t i = start_block; i < end_block; ++i) {
+      size_t n = decompressBlock(i, in, inbuf, outbuf);
+      auto beg = outbuf.cbegin(), end = outbuf.cbegin() + n;
+      if (outbuf.front().mz < min_mz)
+        beg = std::lower_bound(beg, end, min_mz,
+            [](const ims::Peak& p, double mz) { return p.mz < mz; });
+      if (outbuf.back().mz > max_mz)
+        end = std::upper_bound(beg, end, max_mz,
+            [](double mz, const ims::Peak& p) { return mz < p.mz; });
+      result.insert(result.end(), beg, end);
+    }
+    return result;
+  }
 
-	ims::Image<float> image(double mz, double ppm) const {
-		assert(ppm > 0);
+  ims::Image<float> image(double mz, double ppm) const {
+    assert(ppm > 0);
 
-		ims::Image<float> img(height(), width());
-		readImage(mz, ppm, img.rawPtr());
-		return img;
-	}
+    ims::Image<float> img(height(), width());
+    readImage(mz, ppm, img.rawPtr());
+    return img;
+  }
 
-	void readImage(double mz, double ppm, float* image) const {
-		auto idx = [&](size_t x, size_t y) { return ims::pixelIndex(x, y, width()); };
+  void readImage(double mz, double ppm, float* image) const {
+    auto idx = [&](size_t x, size_t y) { return ims::pixelIndex(x, y, width()); };
 
-		for (size_t i = 0; i < height(); ++i)
-			for (size_t j = 0; j < width(); ++j)
-				if (!index_->header.mask.hasSpectrumAt(i, j))
-					image[idx(i, j)] = -1.0;
-				else
-					image[idx(i, j)] = 0.0;
+    for (size_t i = 0; i < height(); ++i)
+      for (size_t j = 0; j < width(); ++j)
+        if (!index_->header.mask.hasSpectrumAt(i, j))
+          image[idx(i, j)] = -1.0;
+        else
+          image[idx(i, j)] = 0.0;
 
-		auto peaks = slice(mz - mz * ppm * 1e-6, mz + mz * ppm * 1e-6);
-		for (auto& peak: peaks)
-			image[idx(peak.coords.x, peak.coords.y)] += peak.intensity;
-	}
+    auto peaks = slice(mz - mz * ppm * 1e-6, mz + mz * ppm * 1e-6);
+    for (auto& peak: peaks)
+      image[idx(peak.coords.x, peak.coords.y)] += peak.intensity;
+  }
 
-	uint32_t height() const { return index_->header.mask.height; }
-	uint32_t width() const { return index_->header.mask.width; }
+  uint32_t height() const { return index_->header.mask.height; }
+  uint32_t width() const { return index_->header.mask.width; }
 };
 
 }
