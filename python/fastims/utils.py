15aec1ae6d48e2113afad051ce506d96d05d176c
Artem Tarasov <artem.tarasov@embl.de>
2016-02-22 16:11:58 +0100

    finally stop this insanity
    moved C++ code into alexandrovteam/ims-cpp repo,
    and the python bindings to cpyMSpec and cpyIMS(?)

--- python/fastims/utils.py
+++
@@ -1,9 +0,0 @@
-import platform
-import os.path
-
-def full_filename(fn):
-  return os.path.abspath(os.path.join(os.path.dirname(__file__), fn)) 
-
-def shared_lib(name):
-  suffixes = {'Windows': 'dll', 'Linux': 'so', 'Darwin': 'dylib'}
-  return 'lib' + name + '.' + suffixes[platform.system()]
